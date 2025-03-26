1bb18278a3adaed2bf664e66feef797bdab231e5
Robin Guyton <Gladiatorrobin@aol.com>
2025-03-25 21:58:22 -0700

    Added views

--- COMP003B.Assignment2/Program.cs
+++ COMP003B.Assignment2/Program.cs
@@ -1,3 +1,9 @@
+/*Author: Robin Guyton
+ * Course: COMP-003B: ASP.NET Core
+ * Faculty Name: Jonathan Cruz
+ * Purpose: To demonstrate an understating of middleware configuration and order, as well as Views.
+ */
+
 namespace COMP003B.Assignment2
 {
     public class Program
@@ -18,10 +24,12 @@ namespace COMP003B.Assignment2
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
+//Middleware sequence
 
             app.UseHttpsRedirection();
             app.UseStaticFiles();
 
+            app.UseMiddleware<Middleware.RequestTrackerMiddleware>();
             app.UseRouting();
 
             app.UseAuthorization();
